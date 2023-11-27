3f42e169ccc5b057bf9b9e58e1007fe3b4275235
Aviral Singh <kalhansaviral@gmail.com>
2023-11-27 14:01:43 +0530

    Modify design

--- packages/ui/src/IssueCard.tsx
+++ packages/ui/src/IssueCard.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Checkbox, Link, Typography } from "@mui/material";
+import { Box, Button, Card, Checkbox, Link, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 
 export function IssueCard(props)
@@ -27,58 +27,58 @@ export function IssueCard(props)
 function StateOne(props)
 {
     return (
-        <Card style={{
-            border: "2px solid black",
-            margin: 10,
-            minHeight: 100,
-            borderRadius: "10px",
-            backgroundColor: "#9ADE7B"
-        }}>
-            
-            <div style={{
-                display: "flex",
-                justifyContent: "space-between",
-                margin: 5
+            <Card style={{
+                border: "2px solid black",
+                margin: 10,
+                minHeight: 80,
+                borderRadius: "10px",
+                backgroundColor: "#9ADE7B"
             }}>
-                <a style={{textDecoration: "none", color: "black", fontWeight: "bolder"}} href={props.props.issueHelper.issueUrl} target="_blank">{props.props.issueHelper.issueTitle}</a>
-                <Checkbox
-                checked={true}
-                color="default"
-                onChange={()=>{
-                    props.props.onUnMarkClick(props.props.issueHelper.issueUrl);
-                }}
-                inputProps={{ 'aria-label': 'controlled' }}
-                />
-            </div>
-        </Card>
+                
+                <div style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    margin: 5
+                }}>
+                    <a style={{textDecoration: "none", color: "black", fontWeight: "bolder"}} href={props.props.issueHelper.issueUrl} target="_blank">{props.props.issueHelper.issueTitle}</a>
+                    <Checkbox
+                    checked={true}
+                    color="default"
+                    onChange={()=>{
+                        props.props.onUnMarkClick(props.props.issueHelper.issueUrl);
+                    }}
+                    inputProps={{ 'aria-label': 'controlled' }}
+                    />
+                </div>
+            </Card>
     );
 }
 
 function StateTwo(props)
 {
     return (
-        <Card style={{
-            border: "2px solid black",
-            margin: 10,
-            borderRadius: "10px",
-            minHeight: 100,
-        }}>
-            
-            <div style={{
-                display: "flex",
-                justifyContent: "space-between",
-                margin: 5
+            <Card style={{
+                border: "2px solid black",
+                margin: 10,
+                borderRadius: "10px",
+                minHeight: 80,
             }}>
-                <a style={{textDecoration: "none", color: "white", fontWeight: "bolder"}} href={props.props.issueHelper.issueUrl} target="_blank">{props.props.issueHelper.issueTitle}</a>
-                <Checkbox
-                checked={false}
-                onChange={()=>{
-                    props.props.onMarkClick(props.props.issueHelper.issueUrl);
-                    // props.setChecked(false);
-                }}
-                inputProps={{ 'aria-label': 'controlled' }}
-                />
-            </div>
-        </Card>
+                
+                <div style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    margin: 5
+                }}>
+                    <a style={{textDecoration: "none", color: "white", fontWeight: "bolder"}} href={props.props.issueHelper.issueUrl} target="_blank">{props.props.issueHelper.issueTitle}</a>
+                    <Checkbox
+                    checked={false}
+                    onChange={()=>{
+                        props.props.onMarkClick(props.props.issueHelper.issueUrl);
+                        // props.setChecked(false);
+                    }}
+                    inputProps={{ 'aria-label': 'controlled' }}
+                    />
+                </div>
+            </Card>
     );
 }
